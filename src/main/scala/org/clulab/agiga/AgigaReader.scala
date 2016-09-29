2084907e84d2b76aa5fd9872986c893a1f4f4872
Gus Hahn-Powell <gushahnpowell@gmail.com>
2016-09-28 20:50:48 -0700

    Refactor
    Use processors 6

---
+++ src/main/scala/org/clulab/agiga/AgigaReader.scala
@@ -0,0 +1,96 @@
+package org.clulab.agiga
+
+import java.io._
+import com.typesafe.config.ConfigFactory
+import com.typesafe.scalalogging.LazyLogging
+import scala.collection.parallel.ForkJoinTaskSupport
+
+
+object AgigaReader extends App with LazyLogging {
+
+  val config = ConfigFactory.load()
+  val inDir = new File(config.getString("inputDir"))
+  val outDir = new File(config.getString("outDir"))
+  val view = config.getString("view")
+  val nthreads = config.getInt("nthreads")
+
+  def mkOutput(f: File, outDir: File, view: String): Unit = {
+
+    // create a Processors Document
+    val doc = toDocument(f.getAbsolutePath)
+
+    // get output representation
+    val output = view.toLowerCase match {
+      case "words" =>
+        doc.sentences.map(_.words.mkString(" ")).mkString("\n")
+      case "tags" =>
+        doc.sentences.map(_.tags.get.mkString(" ")).mkString("\n")
+      case "lemmas" =>
+        doc.sentences.map(_.lemmas.get.mkString(" ")).mkString("\n")
+      case entities if entities == "entities" || entities == "ner" =>
+        doc.sentences.map(_.entities.get.mkString(" ")).mkString("\n")
+      // these are unordered
+      case "lemma-deps" =>
+        doc.sentences.map { s =>
+          val deps = s.dependencies.get
+          val lemmas = s.lemmas.get
+          depsToString(deps, lemmas)
+        }.mkString("\n")
+      case "tag-deps" =>
+        doc.sentences.map { s =>
+          val deps = s.dependencies.get
+          val tags = s.tags.get
+          depsToString(deps, tags)
+        }.mkString("\n")
+      case "entity-deps" =>
+        doc.sentences.map { s =>
+          val deps = s.dependencies.get
+          val entities = s.entities.get
+          depsToString(deps, entities)
+        }.mkString("\n")
+      case "dep" =>
+        doc.sentences.map { s =>
+          val deps = s.dependencies.get
+          val words = s.words
+          depsToString(deps, words)
+        }.mkString("\n")
+    }
+
+    // prepare output file
+    val fName = f.getName.replace(".xml.gz", "")
+    val outFile = new File(s"$outDir/$fName-$view.txt")
+    val pw = new PrintWriter(outFile)
+    // write processed text to file
+    pw.write(output)
+    pw.close()
+    // compress file
+    compress(outFile)
+    // delete uncompress out file
+    outFile.delete
+
+    logger.info(s"Successfully processed ${f.getName}")
+  }
+
+  // create dir if it doesn't exist...
+  outDir.mkdirs()
+
+  logger.info(s"Input: $inDir")
+  logger.info(s"Output folder: $outDir")
+  logger.info(s"View: $view")
+  
+  val files = inDir.listFiles
+    // filter out any non- *.xml.gz files in the directory
+    .filter(_.getName.endsWith(".xml.gz"))
+    // and parallelize the Array of valid Files...
+    .par
+
+  logger.info(s"Files to process: ${files.size}")
+
+  // limit parallelization
+  files.tasksupport = new ForkJoinTaskSupport(new scala.concurrent.forkjoin.ForkJoinPool(nthreads))
+
+  logger.info(s"Threads to use: $nthreads")
+
+  // process files
+  files.foreach(f => mkOutput(f, outDir, view))
+}
---
+++ src/main/scala/org/clulab/agiga/package.scala
@@ -0,0 +1,133 @@
+package org.clulab
+
+import java.io.{File, FileInputStream, FileOutputStream}
+import edu.jhu.agiga.{AgigaPrefs, AgigaSentence, StreamingDocumentReader}
+import org.apache.commons.compress.archivers.ArchiveStreamFactory
+import org.apache.commons.compress.archivers.zip.ZipArchiveEntry
+import org.apache.commons.compress.utils.IOUtils
+import org.clulab.processors.{Document, Sentence}
+import org.clulab.struct.{Edge, DirectedGraph, GraphMap}
+import scala.collection.JavaConverters._
+
+
+package object agiga {
+
+  def compress(outFile: File):Unit = {
+
+    val outName = outFile.getName
+
+    /// archive container
+    val container = new FileOutputStream(new File(s"${outFile.getAbsolutePath}.zip"))
+
+    // create archive stream for attaching files
+    val archive = new ArchiveStreamFactory().createArchiveOutputStream(ArchiveStreamFactory.ZIP, container)
+
+    // write header info
+    archive.putArchiveEntry(new ZipArchiveEntry(outName))
+    // Copy input file
+    IOUtils.copy(new FileInputStream(outFile), archive)
+    // close archive
+    archive.closeArchiveEntry()
+    archive.finish()
+    // close container
+    container.close()
+  }
+
+  /**
+    * Extract Stanford collapsed dependencies from an Agiga sentence
+    */
+  def mkDependencies(s: AgigaSentence): DirectedGraph[String] = {
+    // collapsed dependencies...
+    val collapsedDeps = s.getColCcprocDeps.asScala
+
+    val graphComponents: List[Any] =
+      for {
+        c <- collapsedDeps.toList
+        // component indices for edge construction
+        depIndex = c.getDepIdx
+        headIndex = c.getGovIdx
+        // relation
+        rel = c.getType
+      } yield {
+        headIndex match {
+          case -1 => depIndex
+          case _ => Edge[String](source = headIndex, destination = depIndex, relation = rel)
+        }
+      }
+
+    val edges: List[Edge[String]] = graphComponents
+      .collect { case e: Edge[_] => Edge[String]( source = e.source, destination = e.destination, relation = e.relation.toString) }
+
+    val roots: Set[Int] = graphComponents
+      .collect { case r: Int => r }.toSet
+
+    DirectedGraph[String](edges, roots)
+  }
+
+  /** Converts agiga annotations to a Processors Document
+    * and then generates a text representation of that Document using a specified "view"
+    * view: words, lemmas, tags, entities, etc
+    */
+  def toDocument(filename: String): Document = {
+    // Setup Gigaword API Preferences
+    val prefs = new AgigaPrefs()
+    // label for agiga dependency type
+    prefs.setAll(true)
+    prefs.setWord(true)
+    // Retrieve all gigaword documents contained within a given file
+    val reader = new StreamingDocumentReader(filename, prefs)
+    val sentences = for {
+      agigaDoc <- reader.iterator().asScala
+      s <- agigaDoc.getSents.asScala
+      tokens = s.getTokens.asScala
+      // words
+      words = tokens.map(_.getWord)
+      // lemmas
+      lemmas = tokens.map(_.getLemma)
+      // pos tags
+      posTags = tokens.map(_.getPosTag)
+      // ner labels
+      nerLabels = tokens.map(_.getNerTag)
+      // offsets
+      startOffsets = tokens.map(_.getCharOffBegin)
+      endOffsets = tokens.map(_.getCharOffEnd)
+      deps = mkDependencies(s)
+    } yield {
+
+      Sentence(
+        /** Actual tokens in this sentence */
+        words.toArray,
+        /** Start character offsets for the words; start at 0 */
+        startOffsets.toArray,
+        /** End character offsets for the words; start at 0 */
+        endOffsets.toArray,
+        /** POS tags for words (OPTION) */
+        tags = Some(posTags.toArray),
+        /** Lemmas (OPTION) */
+        lemmas = Some(lemmas.toArray),
+        /** NE labels (OPTION) */
+        entities = Some(nerLabels.toArray),
+        /** Normalized values of named/numeric entities, such as dates (OPTION) */
+        norms = None,
+        /** Shallow parsing labels (OPTION) */
+        chunks = None,
+        /** Constituent tree of this sentence; includes head words Option[Tree[String]] */
+        tree = None,
+        /** *Dependencies */
+        deps = GraphMap(Map(GraphMap.STANFORD_COLLAPSED -> deps))
+      )
+    }
+    new Document(sentences.toArray)
+  }
+
+  /**
+    * Generate a string representation of a sentence's dependencies
+    */
+  def depsToString(deps: DirectedGraph[String], tokens: Array[String]):String = {
+    // include roots in deps representation
+    val rootStr = deps.roots.map(r => s"??_root_${tokens(r)}").mkString(" ")
+    // everything but roots...
+    val depStr = deps.allEdges.map(triple => s"${tokens(triple._1)}_${triple._3}_${tokens(triple._2)}").mkString(" ")
+    s"$depStr $rootStr"
+  }
+}
